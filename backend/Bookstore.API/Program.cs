12a167339e040ffd22986128769cfc62ddf3fd29
brynnesplin <103144915+brynnesplin@users.noreply.github.com>
2025-03-31 20:37:14 -0600

    last commits?

--- backend/Bookstore.API/Program.cs
+++ backend/Bookstore.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "https://icy-bush-0053cf01e.6.azurestaticapps.net/")
+            policy.WithOrigins("http://localhost:3000", "https://icy-bush-0053cf01e.6.azurestaticapps.net")
             .AllowCredentials()
             .AllowAnyHeader()
             .AllowAnyMethod();
